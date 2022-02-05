349f8f394cd9de636439d1000be7b7b01341be30
TENG SHINAN <betterteng@gmail.com>
2022-02-05 18:06:38 +1100

    Create Time-Conversion.js

---
+++ JavaScript/Time-Conversion.js
@@ -0,0 +1,17 @@
+function timeConversion(s) {
+  // Write your code here
+  const modifier = s.slice(-2);
+  const time = s.slice(0, -2);
+
+  let [hours, minutes, seconds] = time.split(':');
+
+  if (hours === '12') {
+    hours = '00';
+  }
+
+  if (modifier === 'PM') {
+    hours = parseInt(hours, 10) + 12;
+  }
+
+  return `${hours}:${minutes}:${seconds}`;
+}
