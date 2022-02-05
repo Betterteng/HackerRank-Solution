7afda23ff21267dfee501d8d0ec0f58ba8f55b7a
TENG SHINAN <betterteng@gmail.com>
2022-02-05 17:57:29 +1100

    Create Diagonal-Difference.js

---
+++ JavaScript/Diagonal-Difference.js
@@ -0,0 +1,12 @@
+function diagonalDifference(arr) {
+  // Write your code here
+  let result1 = 0;
+  let result2 = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    result1 += arr[i][i];
+    result2 += arr[i][arr.length - i - 1]
+  }
+
+  return Math.abs(result1 - result2);
+}
